ba8e95d11b15a7d00660c6c66f3c2b7a24e859ce
Joshua Onabanjo <joshua@memrise.com>
2019-10-22 20:02:29 +0100

    Added Room to store github repos into database - Created Github Repo DAO - Created a database which allows always testing

---
+++ app/src/main/java/com/marannix/android/cadence/data/GithubRepoDao.kt
@@ -0,0 +1,16 @@
+package com.marannix.android.cadence.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.marannix.android.cadence.model.GitHubRepoModel
+
+@Dao
+interface GithubRepoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertGithubRepos(githubRepos: List<GitHubRepoModel>)
+
+    @Query("select * from repos")
+    fun getAllGithubRepos(): List<GitHubRepoModel>
+}
--- app/src/main/java/com/marannix/android/cadence/model/GitHubRepoModel.kt
+++ app/src/main/java/com/marannix/android/cadence/model/GitHubRepoModel.kt
@@ -1,5 +1,8 @@
 package com.marannix.android.cadence.model
 
+import androidx.room.Entity
+
+@Entity(tableName = "repos")
 data class GitHubRepoModel(
     val name: String
 )
