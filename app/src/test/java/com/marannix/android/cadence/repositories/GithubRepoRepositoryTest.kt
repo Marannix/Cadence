16bb78afc6f9b51223bce09dd66ad46c7c5f40dc
Josh <onabanjotobi@gmail.com>
2019-10-25 23:07:31 +0100

    Added a test for correct cache response when network fails
    I noticed that the test succeed but it seems I'll need to find another alternative to handle faking a network error.

--- app/src/test/java/com/marannix/android/cadence/repositories/GithubRepoRepositoryTest.kt
+++ app/src/test/java/com/marannix/android/cadence/repositories/GithubRepoRepositoryTest.kt
@@ -1,6 +1,5 @@
 package com.marannix.android.cadence.repositories
 
-import androidx.lifecycle.MutableLiveData
 import com.marannix.android.cadence.api.GithubRepoApi
 import org.junit.Before
 import org.junit.Test
@@ -8,15 +7,10 @@ import com.google.gson.GsonBuilder
 import com.marannix.android.cadence.data.GithubRepoDao
 import com.marannix.android.cadence.model.GitHubRepoModel
 import com.marannix.android.cadence.utils.UnitTestUtils.Companion.readJsonFile
-import io.reactivex.FlowableSubscriber
 import io.reactivex.Single
-import io.reactivex.disposables.Disposable
-import io.reactivex.observers.BaseTestConsumer
-import io.reactivex.observers.TestObserver
 import org.assertj.core.api.Assertions.assertThat
-import org.mockito.Mockito
 import org.mockito.Mockito.*
-
+import retrofit2.HttpException
 
 class GithubRepoRepositoryTest {
 
@@ -40,7 +34,7 @@ class GithubRepoRepositoryTest {
     @Test
     fun `when calling the network, parse correctly the response`() {
         `when`(api.getRepos()).thenReturn(Single.just(githubrepoResponse))
-        `when`(dao.getGithubRepos()).thenReturn(Single.just(githubrepoResponse))
+        `when`(dao.getGithubRepos()).thenReturn(Single.just(emptyList()))
 
         repository.getGithubRepos().subscribe { result = it }
 
@@ -49,6 +43,25 @@ class GithubRepoRepositoryTest {
         assertThat(result.last()).isEqualTo(githubrepoResponse.last())
 
         verify(api).getRepos()
+    }
+
+    @Test
+    fun `when network fails, parse correctly the response from cache`() {
+        // TODO: Test passes but returns exception due to throwable >.>
+        //  *The exception was not handled due to missing onError handler in the subscribe() method call.*
+        // The idea is to make the api request fail and retrieve data from the cache response
+        // Tried using Single.just(emptyList()) however the response becomes empty, hmmm...
+
+        `when`(api.getRepos()).thenReturn(Single.error(Throwable("")))
+        `when`(dao.getGithubRepos()).thenReturn(Single.just(githubrepoResponse))
+
+        repository.getGithubRepos().subscribe { result = it }
+
+        assertThat(result.size).isEqualTo(githubrepoResponse.size)
+        assertThat(result.first()).isEqualTo(githubrepoResponse.first())
+        assertThat(result.last()).isEqualTo(githubrepoResponse.last())
+
+        verify(dao).getGithubRepos()
     }
 }
 
